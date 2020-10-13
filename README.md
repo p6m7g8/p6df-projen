42f3b6b8223325264ebff122e3057e635be68d29
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-10-13 13:11:08 -0400

    initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+### init.zsh:
+- p6df::modules::projen::deps()
+- p6df::modules::projen::init()
+- p6df::modules::projen::langs()
+
---
+++ init.zsh
@@ -0,0 +1,39 @@
+######################################################################
+#<
+#
+# Function: p6df::modules::projen::deps()
+#
+#>
+######################################################################
+p6df::modules::projen::deps() {
+    ModuleDeps=(
+      p6m7g8/p6df-node
+      # eladb/projen # (segfault???)
+      # pgollucci/projen
+    )
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::projen::langs()
+#
+#>
+######################################################################
+p6df::modules::projen::langs() {
+
+  (cd $P6_DFZ_SRC_DIR/pgollucci/projen ; npm run projen)
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::projen::init()
+#
+#>
+######################################################################
+p6df::modules::projen::init() {
+
+  alias pjp="$P6_DFZ_SRC_DIR/pgollucci/projen/bin/projen"
+  alias pje="$P6_DFZ_SRC_DIR/eladb/projen/bin/projen"
+}
